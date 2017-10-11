8a90ad78c89e48d536f61d1bf2922afffe608167
carolyn arnold <carolyn.d.arnoldlp@gmail.com>
2017-10-11 23:39:02 +0000

    Added deposits, withdrawals, and transfer to bank account.

--- bank_account.rb
+++ bank_account.rb
@@ -4,4 +4,17 @@ class BankAccount
 	def initialize(opening_balance, account_holder)
 	  @balance = opening_balance
   end
+    
+    def deposit(amount)
+        @balance += amount
+    end
+    
+    def withdrawal(amount)
+        @balance -= amount
+    end
+    
+    def transfer(amount, account)
+        withdrawal(amount)
+        account.deposit(amount)
+    end
 end
